155c5207d5aba8b1f8ef2f92c848730967f309de
Kevin Wynn <kevin@outboundengine.com>
2016-03-07 18:52:33 -0600

    pulling sort out to components

---
+++ components/sort.php
@@ -0,0 +1,7 @@
+<div class="col-md-10 col-md-offset-1 sort" id="sortMovies">
+  <a id="sortAction">Sort Movies <i class="fa fa-sort"></i></a>
+  <ul id="sortItems" class="sort-dropdown">
+    <li id="sortReviewed">Recently Reviewed</li>
+    <li id="sortReleased">Recently Released</li>
+  </ul>
+</div>
--- index.php
+++ index.php
@@ -24,13 +24,7 @@
     </div>
   </div>
   <div class="container-fluid recent">
-    <div class="col-md-10 col-md-offset-1 sort" id="sortMovies">
-      <a id="sortAction">Sort Movies <i class="fa fa-sort"></i></a>
-      <ul id="sortItems" class="sort-dropdown">
-        <li id="sortReviewed">Recently Reviewed</li>
-        <li id="sortReleased">Recently Released</li>
-      </ul>
-    </div>
+    <?php include 'components/sort.php' ?>
     <div class="col-md-10 col-md-offset-1" id="recentMovies">
     </div>
   </div>
