505f571108cb92d607d31768caedb3d3821756a0
Kevin Wynn <kevin@outboundengine.com>
2016-03-07 18:15:42 -0600

    fixing individual page hero image

--- js/page.js
+++ js/page.js
@@ -18,53 +18,6 @@ $(document).ready(function() {
   
   $.ajax({
     type: "GET",
-    url: "../php/heroImage.php",             
-    dataType: "json",                
-    success: function(result) {
-      // grab content from json returned
-      var backdrop = prefixUrl + result[0].backdrop_path,
-          title = result[0].movie_title,
-          genres = result[0].genre,
-          overview = result[0].overview,
-          score = result[0].score,
-          director = result[0].director,
-          cast = result[0].cast;
-      
-      // build out and inject html for injection
-      heroImage.css('background-image', 'url('+backdrop+')');
-      $('.footer').css('background-image', 'url('+backdrop+')');
-      movieTitle.html(title);
-      movieGenres.html(genres);
-      movieOverview.html(overview);
-      movieCast.html(cast);
-      movieDirector.html('<span class="intro-text">Directed By - </span>' + director);
-      movieCast.html('<span class="intro-text">Starring - </span>');
-      
-      cast = cast.replace(/ /g,'');
-      cast = cast.split(',');
-      
-      for(i=0; i<cast.length; i++){
-        theMovieDb.people.getById({"id":cast[i]}, parseCast, errorCB);
-      }
-      
-      function parseCast(data){
-          var name = $.parseJSON(data).name;
-        
-          if(wave == cast.length) {
-            movieCast.append(name);
-          } else {
-            movieCast.append(name + ', ');
-          }
-      }
-      
-      for(i=0; i<score; i++){
-        movieScore.append('<i class="fa fa-star"></i> ');
-      }
-    }
-  });
-  
-  $.ajax({
-    type: "GET",
     url: "../php/homeReviewed.php",             
     dataType: "json",                
     success: function(result) {
