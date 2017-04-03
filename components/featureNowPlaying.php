3d9837b3b0dd1667da6e84cb50b83debb695fec7
Kevin Wynn <lukevinskywynn@gmail.com>
2017-04-02 22:19:56 -0500

    add assets

---
+++ components/featureNowPlaying.php
@@ -0,0 +1,4 @@
+<div class="col-md-10 col-md-offset-1">
+   <h4>Featured Now Playing Movie</h4>
+   <div id="featuredNowPlaying" class="col-md-12"></div>
+</div>
---
+++ php/homeFeatureNowPlaying.php
@@ -0,0 +1,30 @@
+<?php include '../admin/includes/connection.php' ?>
+<?php
+
+  connect();
+
+  $json = array();
+  $result = mysql_query("SELECT * FROM nowplaying WHERE view_on_home=1 LIMIT 1");
+
+  while($row = mysql_fetch_array($result))
+  {
+    $bus = array(
+      'movie_title' => $row['movie_title'],
+      'overview' => $row['overview'],
+      'director' => $row['director'],
+      'cast' => $row['cast'],
+      'poster_path' => $row['poster_path'],
+      'backdrop_path' => $row['backdrop_path'],
+      'release_date' => $row['release_date'],
+      'popular_vote' => $row['popular_vote'],
+      'genre' => $row['genre'],
+      'moviedb_id' => $row['moviedb_id']
+    );
+    array_push($json, $bus);
+  }
+
+  $jsonstring = json_encode($json);
+  echo $jsonstring;
+
+  die();
+?>
